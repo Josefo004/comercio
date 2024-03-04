aabf7e914470d7d786b89b1f81f66c3437d5972a
Josefo004 <ig_jose@msn.com>
2024-03-04 01:54:44 -0400

    consumir servicio v1

---
+++ frontend/controllers/ApiController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace frontend\controllers;
+
+use Yii;
+use yii\web\Controller;
+use yii\httpclient\Client;
+
+use common\models\Ordenes;
+
+class ApiController extends Controller
+{
+  public function actionConsumeApi()
+  {
+    // URL del servicio REST que deseas consumir
+    $apiUrl = 'http://localhost:3000/posts';
+
+    // Crear una instancia del cliente HTTP
+    $client = new Client();
+
+    // Realizar la solicitud GET al servicio REST
+    $response = $client->createRequest()
+      ->setMethod('GET')
+      ->setUrl($apiUrl)
+      ->send();
+
+    // Verificar si la solicitud fue exitosa (código de estado 200)
+    if ($response->isOk) {
+      // Decodificar la respuesta JSON
+      $data = $response->data;
+      dd($data);
+      // Trabajar con los datos recibidos
+      // Por ejemplo, pasar los datos a la vista
+      return $this->render('view', ['data' => $data]);
+    } else {
+      // Si la solicitud no fue exitosa, manejar el error apropiadamente
+      Yii::error('Error al consumir el servicio REST: ' . $response->statusCode);
+      // Por ejemplo, mostrar un mensaje de error
+      throw new \yii\web\HttpException($response->statusCode, 'Error al consumir el servicio REST');
+    }
+  }
+
+  public function actionObtenerQr($IdOrden)
+  {
+    $orden = Ordenes::find()
+          ->joinWith('creador')
+          ->where(['Ordenes.IdOrden' => $IdOrden])
+          ->one();
+    //dd($orden);
+
+    $solQr = $this->crearSolicitudQr( $orden );
+    //dd($solQr);
+    // URL del servicio REST que deseas consumir
+    $apiUrl = 'http://172.16.1.251/pagos/pagos-qr/obtener-qr';
+    
+    // Crear una instancia del cliente HTTP
+    $client = new Client();
+
+    // Realizar la solicitud POST al servicio REST con los datos en formato JSON
+    $response = $client->createRequest()
+      ->setMethod('POST')
+      ->setUrl($apiUrl)
+      ->setData($solQr) // Configurar los datos a enviar
+      ->setFormat(Client::FORMAT_JSON) // Especificar el formato JSON
+      ->send();
+    
+    // Verificar si la solicitud fue exitosa (código de estado 200)
+    if ($response->isOk) {
+      // Capturar la respuesta del servicio
+      $responseData = $response->getData(); // Obtiene la respuesta en formato de array
+      dd($responseData);
+    }
+    else{
+      // Si la solicitud no fue exitosa, manejar el error apropiadamente
+      Yii::error('Error al enviar datos al servicio REST: ' . $response->statusCode);
+      // Por ejemplo, mostrar un mensaje de error
+      throw new \yii\web\HttpException($response->statusCode, 'Error al enviar datos al servicio REST');
+    }
+  }
+
+  protected function crearSolicitudQr($ord) {
+    //dd($ord);
+    $code = $this->completarCeros($ord->IdOrden);
+    $cod2 = $code.'-'.$ord->creador->IdPersona;
+    $fecha = date('Y-m-d', strtotime($ord->FechaCreacion));
+    $fecha2 = new \DateTime($fecha); 
+    $fecha2->modify('+2 days');
+    $fecha2 = $fecha2->format('d/m/Y');
+    $re = [
+      'datos' => [
+        'monto' => $ord->TotalOrden,
+        'referencia' => 'pagoOrden'.$cod2,
+        'codigoPago' => 'VEN'.$cod2,
+        'nombreCompleto' => $ord->NombreCompleto,
+        'item' => [
+          'idOrden' => $code,
+          'nroComprobante' => '0',
+          'fechaPago' => $fecha,
+          'FechaLimiteAtencion' => $fecha2
+        ],
+      ]
+    ];
+
+    return $re;
+  }
+
+  protected function completarCeros($nu) {
+    $ta = strval($nu); //texto auxiliar
+    $lg = strlen($ta);
+    $cc = '0';
+    for ($i=1; $lg + 3 > $i  ; $i++) { $cc .= '0'; }
+    return '-'.$cc.$ta;
+  }
+}
--- frontend/controllers/CarritoController.php
+++ frontend/controllers/CarritoController.php
@@ -14,211 +14,235 @@ use yii\helpers\ArrayHelper;
 
 use Yii;
 use yii\web\Controller;
+
 class CarritoController extends Controller
 {
 
-    public function asArray1($data){
-        $data = ArrayHelper::map($data,'IdProductoTalla', function($data, $defaultValue){
-            //return $data->idTalla->Talla.' '.$data->idTalla->DescripcionTalla.' - cant. '.$data->Cantidad;
-            return '"'.$data->idTalla->Talla.'" '.$data->idTalla->DescripcionTalla;
-        });
-        return $data;
-    }
+  public function asArray1($data)
+  {
+    $data = ArrayHelper::map($data, 'IdProductoTalla', function ($data, $defaultValue) {
+      //return $data->idTalla->Talla.' '.$data->idTalla->DescripcionTalla.' - cant. '.$data->Cantidad;
+      return '"' . $data->idTalla->Talla . '" ' . $data->idTalla->DescripcionTalla;
+    });
+    return $data;
+  }
 
-    public function addToCarrito($data){
-        $alCarrito = ArrayHelper::toArray($data);
-        $mg = !Yii::$app->session->has('carrito')? [] : Yii::$app->session->get('carrito');
-        array_push($mg,$alCarrito);
-        Yii::$app->session->set('carrito', $mg);
-        $aux = Yii::$app->session->get('carrito');
-    }
+  public function addToCarrito($data)
+  {
+    $alCarrito = ArrayHelper::toArray($data);
+    $mg = !Yii::$app->session->has('carrito') ? [] : Yii::$app->session->get('carrito');
+    array_push($mg, $alCarrito);
+    Yii::$app->session->set('carrito', $mg);
+    $aux = Yii::$app->session->get('carrito');
+  }
 
-    public function actionIndex($id, $tprecio=null)
-    {
+  public function actionIndex($id, $tprecio = null)
+  {
     /// revisar
+  }
+
+  //vizualizamos una orden
+  public function actionVerOrden($idOrden)
+  {
+    $orden = Ordenes::find()
+      ->joinWith('estado')
+      ->joinWith('detallesOrden')
+      ->joinWith('creador')
+      ->where(['Ordenes.IdOrden' => $idOrden])
+      ->one();
+    return $this->render('orden', ['orden' => $orden]);
+  }
+
+  //mandamos el identificador del producto y el tipo de precio al cual queremos procesar el pedido
+  public function actionCreate($id, $tprecio = null)
+  {
+    $producto = $this->findModel2($id);
+    $tallas = $this->findTallas($id);
+    $tallas = $this->asArray1($tallas);
+    //dd($tallas);
+    $modeloCarrito = new CarritoForm();
+
+    if ($this->request->isPost) {
+      $modeloCarrito->load($this->request->post());
+      $modeloCarrito->FechaHoraRegistro = CommonQueries::GetFechaHoraActual();
+      $this->addToCarrito($modeloCarrito);
+
+      return $this->render('create', [
+        'producto' => $producto,
+        'tallas' => $tallas,
+        'tprecio' => $tprecio,
+        'modeloCarrito' => $modeloCarrito,
+        'modal' => 1,
+      ]);
     }
 
-    //vizualizamos una orden
-    public function actionVerOrden($idOrden)
-    {
-        $orden = Ordenes::find()
-                        ->joinWith('estado')
-                        ->joinWith('detallesOrden')
-                        ->joinWith('creador')
-                        ->where(['Ordenes.IdOrden' => $idOrden])
-                        ->one();
-        return $this->render('orden', ['orden' => $orden]);
-    }
+    return $this->render('create', [
+      'producto' => $producto,
+      'tallas' => $tallas,
+      'tprecio' => $tprecio,
+      'modeloCarrito' => $modeloCarrito,
+      'modal' => 0,
+    ]);
+  }
 
-    //mandamos el identificador del producto y el tipo de precio al cual queremos procesar el pedido
-    public function actionCreate($id, $tprecio=null)
-    {
-        $producto = $this->findModel2($id);
-        $tallas = $this->findTallas($id);
-        $tallas = $this->asArray1($tallas);
-        //dd($tallas);
-        $modeloCarrito = new CarritoForm();
+  public function actionShow()
+  {
+    if (Yii::$app->session->has('carrito')) {
+      $modeloOrden = new OrdenForm();
+      if ($this->request->isPost) {
+        //return $this->redirect(['api/consume-api']);
+        //return $this->redirect(['ver-orden', 'idOrden' => 4]);
+        $modeloOrden->load($this->request->post());
 
-        if ($this->request->isPost) {
-            $modeloCarrito->load($this->request->post());
-            $modeloCarrito->FechaHoraRegistro = CommonQueries::GetFechaHoraActual();
-            $this->addToCarrito($modeloCarrito);
-            
-            return $this->render('create', [
-                'producto' => $producto,
-                'tallas' => $tallas,
-                'tprecio' => $tprecio,
-                'modeloCarrito' => $modeloCarrito,
-                'modal' => 1,
-            ]);
-        }
-       
-        return $this->render('create', [
-            'producto' => $producto,
-            'tallas' => $tallas,
-            'tprecio' => $tprecio,
-            'modeloCarrito' => $modeloCarrito,
-            'modal' => 0,
-        ]);
-    }
+        // recupero el usuario o guardo un nuevo usuario 
+        $usuario = ($modeloOrden->CodigoUsuario != '') ? Usuarios::findIdentity($modeloOrden->CodigoUsuario) : $usuario = $this->crearUsuario($modeloOrden);
 
-    public function actionShow(){
-        if (Yii::$app->session->has('carrito')) {
-            $modeloOrden = new OrdenForm();
-            if ($this->request->isPost) {
-                //return $this->redirect(['ver-orden', 'idOrden' => 4]);
-                $modeloOrden->load($this->request->post());
+        // Creamos una Orden 
+        $orden = $this->crearOrden($usuario, $modeloOrden);
                 
-                // recupero el usuario o guardo un nuevo usuario 
-                $usuario = ($modeloOrden->CodigoUsuario!='') ? Usuarios::findIdentity($modeloOrden->CodigoUsuario) : $usuario = $this->crearUsuario($modeloOrden); 
-    
-                // Creamos una Orden 
-                $orden = $this->crearOrden($usuario, $modeloOrden);
-                
-                // Detalle de las ordenes
-                $detalleOrd = $this->crearDetallarOrden($orden->IdOrden);
-                // dd($detalleOrd);
-    
-                return $this->redirect(['ver-orden', 'idOrden' => $orden->IdOrden]);
-            }
-            return $this->render('show',[
-                'modeloOrden' => $modeloOrden,
-            ]);
+        // dd($solQr);
+        // return $this->redirect(['api/obtener-qr', 'IdOrden' => 2]);
+
+        // Detalle de las ordenes
+        $detalleOrd = $this->crearDetallarOrden($orden->IdOrden);
+        // dd($detalleOrd);
+
+        if ($detalleOrd) {
+          //Mandamos la Orden a la API para crear el QR
+          return $this->redirect(['api/obtener-qr', 'IdOrden' => $orden->IdOrden]);
         }
-        else {
-            return $this->redirect(['site/index']);
-        }
-    }
 
-    protected function findModel($IdProducto)
-    {
-        if (($model = Productos::findOne(['IdProducto' => $IdProducto])) !== null) {
-            return $model;
-        }
-    }
 
-    public function actionEliminar($id)
-    {
-        $carritot = Yii::$app->session->get('carrito'); //carrito temporal
-        //dd($carritot);
-        unset($_SESSION['carrito']);
-        unset($carritot[$id]);
-        if (count($carritot)==0) {
-            return $this->redirect(['site/index']);
-        }    
-        Yii::$app->session->set('carrito', $carritot);
-        // return $this->render('show');
-        return $this->redirect(['site/index','ttt'=>1]);
+        //return $this->redirect(['ver-orden', 'idOrden' => $orden->IdOrden]);
+      }
+      return $this->render('show', [
+        'modeloOrden' => $modeloOrden,
+      ]);
+    } else {
+      return $this->redirect(['site/index']);
     }
+  }
 
-    public function actionEliminar2($id)
-    {
-        $carritot = Yii::$app->session->get('carrito');
-        
-        unset($_SESSION['carrito']);
-        unset($carritot[$id]);
-        if (count($carritot)==0) {
-            return $this->redirect(['site/index']);
-        }    
-        Yii::$app->session->set('carrito', $carritot);
-        return $this->redirect(['show']);
+  protected function findModel($IdProducto)
+  {
+    if (($model = Productos::findOne(['IdProducto' => $IdProducto])) !== null) {
+      return $model;
     }
+  }
 
-    protected function findModel2($IdProducto){
-        $model = Productos::find()
-                        ->joinWith('codigoEstado')
-                        ->joinWith('idCategoriaGenero')
-                        ->joinWith('idCategoriaProducto')
-                        ->where(['=','Productos.IdProducto',$IdProducto])
-                        ->one();
-        return $model;
+  public function actionEliminar($id)
+  {
+    $carritot = Yii::$app->session->get('carrito'); //carrito temporal
+    //dd($carritot);
+    unset($_SESSION['carrito']);
+    unset($carritot[$id]);
+    if (count($carritot) == 0) {
+      return $this->redirect(['site/index']);
     }
+    Yii::$app->session->set('carrito', $carritot);
+    // return $this->render('show');
+    return $this->redirect(['site/index', 'ttt' => 1]);
+  }
 
-    protected function findTallas($IdProducto){
-        $model = ProductoTallas::find()
-                        ->joinWith('idTalla')
-                        ->where(['=','IdProducto',$IdProducto])
-                        ->andwhere(['>','Cantidad',0])
-                        ->all();
-        return $model;
-    }
+  public function actionEliminar2($id)
+  {
+    $carritot = Yii::$app->session->get('carrito');
 
-    protected function crearUsuario($modeloOrden){
-        $user = new Usuarios();
-        $user->NombreCompleto = $modeloOrden->NombreCompleto;
-        $user->Email = $modeloOrden->Email;
-        $user->IdPersona = $modeloOrden->IdPersona;
-        $user->Celular = $modeloOrden->Celular;
-        $user->generateCodigoUsuario();
-        $user->Login = $user->CodigoUsuario;
-        $user->setPassword($modeloOrden->IdPersona);
-        $user->generateAuthKey();
-        $user->generateEmailVerificationToken();
-        $user->Estado = 'V';
-       
-        //return $user->save() && $this->sendEmail($user);
-       
-        $user->save();
-        return $user;
+    unset($_SESSION['carrito']);
+    unset($carritot[$id]);
+    if (count($carritot) == 0) {
+      return $this->redirect(['site/index']);
     }
+    Yii::$app->session->set('carrito', $carritot);
+    return $this->redirect(['show']);
+  }
 
-    protected function crearOrden($usuario, $modeloOrden){
-        $orden = new Ordenes();
-        $orden->CodigoUsuarioCreacion = $usuario->CodigoUsuario;
-        $orden->NombreCompleto = mb_strtoupper($modeloOrden->NombreCompleto);
-        $orden->Celular = $modeloOrden->Celular;
-        $orden->Email = $modeloOrden->Email;
-        $orden->CodigoEstado = 'P';
-        $orden->TotalOrden = $modeloOrden->TotalOrden;
-        $orden->FechaCreacion = CommonQueries::GetFechaHoraActual();
-        //dd($orden, $orden->save());
-        $orden->save();
-        //$errors = $orden->getErrors();
-        //dd($errors);
-        return $orden;
-    }
+  protected function findModel2($IdProducto)
+  {
+    $model = Productos::find()
+      ->joinWith('codigoEstado')
+      ->joinWith('idCategoriaGenero')
+      ->joinWith('idCategoriaProducto')
+      ->where(['=', 'Productos.IdProducto', $IdProducto])
+      ->one();
+    return $model;
+  }
 
-    protected function crearDetallarOrden($idOrden = 0){
-        $tmpCarrito = Yii::$app->session->get('carrito');
-        //dd($tmpCarrito);
-        $re = true;
-        foreach ($tmpCarrito as $value) {
-            $detalleO = new DetalleOrdenes();
-            $detalleO->IdOrden = $idOrden;
-            $detalleO->IdProducto = $value['IdProducto'];
-            $detalleO->IdProductoTalla = $value['IdProductoTalla'];
-            $detalleO->Talla = $value['Talla'];
-            $detalleO->CodigoProducto = $value['CodigoProducto'];
-            $detalleO->ProductoPara = $value['ProductoPara'];
-            $detalleO->NombreProducto = $value['NombreProducto'];
-            $detalleO->Imagen = $value['Imagen'];
-            $detalleO->Precio = $value['Precio'];
-            $detalleO->Cantidad = $value['Cantidad'];
-            $detalleO->Total = $value['Total'];
-            $detalleO->FechaRegistro = $value['FechaHoraRegistro'];
-            if(!$detalleO->save()){$re = false; break;}
-        }
-        if ($re) { unset($_SESSION['carrito']); }
-        return $re;
+  protected function findTallas($IdProducto)
+  {
+    $model = ProductoTallas::find()
+      ->joinWith('idTalla')
+      ->where(['=', 'IdProducto', $IdProducto])
+      ->andwhere(['>', 'Cantidad', 0])
+      ->all();
+    return $model;
+  }
+
+  protected function crearUsuario($modeloOrden)
+  {
+    $user = new Usuarios();
+    $user->NombreCompleto = $modeloOrden->NombreCompleto;
+    $user->Email = $modeloOrden->Email;
+    $user->IdPersona = $modeloOrden->IdPersona;
+    $user->Celular = $modeloOrden->Celular;
+    $user->generateCodigoUsuario();
+    $user->Login = $user->CodigoUsuario;
+    $user->setPassword($modeloOrden->IdPersona);
+    $user->generateAuthKey();
+    $user->generateEmailVerificationToken();
+    $user->Estado = 'V';
+
+    //return $user->save() && $this->sendEmail($user);
+
+    $user->save();
+    return $user;
+  }
+
+  protected function crearOrden($usuario, $modeloOrden)
+  {
+    $orden = new Ordenes();
+    $orden->CodigoUsuarioCreacion = $usuario->CodigoUsuario;
+    $orden->NombreCompleto = mb_strtoupper($modeloOrden->NombreCompleto);
+    $orden->Celular = $modeloOrden->Celular;
+    $orden->Email = $modeloOrden->Email;
+    $orden->CodigoEstado = 'P';
+    $orden->TotalOrden = $modeloOrden->TotalOrden;
+    $orden->FechaCreacion = CommonQueries::GetFechaHoraActual();
+    //dd($orden, $orden->save());
+    $orden->save();
+    //$errors = $orden->getErrors();
+    //dd($errors);
+    return $orden;
+  }
+
+  protected function crearDetallarOrden($idOrden = 0)
+  {
+    $tmpCarrito = Yii::$app->session->get('carrito');
+    //dd($tmpCarrito);
+    $re = true;
+    foreach ($tmpCarrito as $value) {
+      $detalleO = new DetalleOrdenes();
+      $detalleO->IdOrden = $idOrden;
+      $detalleO->IdProducto = $value['IdProducto'];
+      $detalleO->IdProductoTalla = $value['IdProductoTalla'];
+      $detalleO->Talla = $value['Talla'];
+      $detalleO->CodigoProducto = $value['CodigoProducto'];
+      $detalleO->ProductoPara = $value['ProductoPara'];
+      $detalleO->NombreProducto = $value['NombreProducto'];
+      $detalleO->Imagen = $value['Imagen'];
+      $detalleO->Precio = $value['Precio'];
+      $detalleO->Cantidad = $value['Cantidad'];
+      $detalleO->Total = $value['Total'];
+      $detalleO->FechaRegistro = $value['FechaHoraRegistro'];
+      if (!$detalleO->save()) {
+        $re = false;
+        break;
+      }
     }
+    if ($re) {
+      unset($_SESSION['carrito']);
+    }
+    return $re;
+  }
+
 }
